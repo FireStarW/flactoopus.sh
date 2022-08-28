d56f0620c9cd05a051590238ec8eb7bff5d64863
FireStarW <firestarw@gmail.com>
2022-08-28 17:54:39 -0400

    add some prefix and suffix clearing, downscale only bigger images

--- flactoopus192-full.sh
+++ flactoopus192-full.sh
@@ -15,20 +15,42 @@ dir=$(pwd)
 parentname="$(dirname "$dir")"
 artistname=$(basename "$parentname")
 albumname=$(basename "$PWD")
-convert $var  -resize 800x800  resize_cover.jpg
-echo "Test output:"
-echo "$artistname - $albumname"
-mkdir "$artistname - $albumname"
 
+#only downscale if res bigger than 800
+convert $var  -resize 800x800\>  resize_cover.jpg
+
+echo "artistname - albumname:"
+echo "$artistname - $albumname"
+
+flacprefix='[FLAC]'
+#remove [FLAC]
+if [[ "$albumname" == *"$flacprefix"* ]];then
+  echo "Flac Suffix is there."
+  albumname=${albumname/FLAC*/}
+  albumname=${albumname%??}
+  echo "$albumname"
+fi
+#remove later
+if [[ "$artistname" == ".lAteR" ]]; then
+foldername=$albumname
+  echo "later prefix is there."
+else
+foldername="$artistname - $albumname"
+fi 
+
+echo "foldername:"
+echo "$foldername"
+
+mkdir "$foldername"
 
 for a in ./*.flac; do
   ffmpeg -i "$a" -b:a 192000 "${a[@]/%flac/opus}" &
 done
 
 wait
-mv *.opus "$artistname - $albumname"
-mv resize_cover.jpg "$artistname - $albumname"
-cd "$artistname - $albumname"
+mv *.opus "$foldername"
+mv resize_cover.jpg "$foldername"
+cd "$foldername"
 kid3-cli -c "select all" -c 'set picture:"resize_cover.jpg" "resize_cover.jpg"' "*.opus"
 rm resize_cover.jpg
 
@@ -39,5 +61,6 @@ if (( file_size < 500000 )); then
 	echo "!! WARNING: File size of $a is $file_size bytes !!"
 fi
 done
+
 
 echo "Done"
